51d5abe0c1820ddae13a27fa32e43e125ff643dd
Kuzkin Ilya <heartsong_@mail.ru>
2024-06-10 20:04:19 +0500

    добавить измененный файл recipients/urls.py

--- recipients/urls.py
+++ recipients/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from recipients.apps import RecipientsConfig
 from recipients.views import RecipientTemplateView, RecipientCreateView, RecipientListView, RecipientDetailView, \
@@ -10,7 +11,7 @@ urlpatterns = [
     path('', RecipientTemplateView.as_view(), name='index'),
     path('list/', RecipientListView.as_view(), name='list'),
     path('create/', RecipientCreateView.as_view(), name='create'),
-    path('<int:pk>/', RecipientDetailView.as_view(), name='view'),
+    path('<int:pk>/', cache_page(60)(RecipientDetailView.as_view()), name='view'),
     path('<int:pk>/update/', RecipientUpdateView.as_view(), name='edit'),
     path('<int:pk>/delete/', RecipientDeleteView.as_view(), name='delete'),
-    ]
+]
