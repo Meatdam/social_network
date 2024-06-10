958feb409d99646a5034cff607dcd2931d7fe32e
Kuzkin Ilya <heartsong_@mail.ru>
2024-06-10 20:00:50 +0500

    добавить измененный файл recipients/models.py pep8

--- recipients/models.py
+++ recipients/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 
+from users.models import User
+
+NULLABLE = {'blank': True, 'null': True}
+
 
 class Recipients(models.Model):
     """
-    Моедль для получателей
+    Модель клиентов, которым будет направлена рассылка
     """
     email = models.EmailField(verbose_name='Почта', unique=True)
     name = models.CharField(max_length=150, verbose_name='Имя')
     description = models.TextField(blank=True, null=True, verbose_name='Сообщение')
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='Менеджер рассылок', **NULLABLE)
 
     def __str__(self):
         return f'{self.email} - {self.name}'
 
     class Meta:
-        verbose_name = 'Получатель'
-        verbose_name_plural = 'Получатели'
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
